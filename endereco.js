f41648156998cb34d31dd74ea3655bda966acbba
Maria Yanagui <maria.yanagui@alunoescolassociais.org.br>
2024-09-20 08:45:51 -0300

    começo do javascript

---
+++ endereco.js
@@ -0,0 +1,4 @@
+'use strict'; // Modo restrito
+// Este modo faz com que o Javascript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação 
+// * Consumo de API - https://viacep.com.br/ */
+
